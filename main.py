fc92b9f545d95e0abffd3fa14872991b9eedab6a
Anton <freelanset@gmail.com>
2025-04-22 11:35:10 +0800

    Code improved

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import requests
 
 
-def get_superheroes_database():
-    url = "https://akabab.github.io/superhero-api/api/all.json"
-    superheroes_database = requests.get(url).json()
-    return superheroes_database
-
-
 def choose_smartest_superhero(compared_superheroes):
-    superheroes_database = get_superheroes_database()
+    url = "https://akabab.github.io/superhero-api/api/all.json"
+    superheroes = requests.get(url).json()
     result = []
-    for superhero in superheroes_database:
+    for superhero in superheroes:
         if superhero['name'] in compared_superheroes:
             info = (superhero['name'], superhero['powerstats']['intelligence'])
             result.append(info)
